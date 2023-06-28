1a4db0418e67f2e75b7bbc32e66fdf57fcdbc0f6
chen <2535705423@qq.com>
2023-06-28 11:58:45 +0800

    my segnext model --2

---
+++ mmseg-segnext-cc.py
@@ -0,0 +1,471 @@
+import torch
+import torch.nn as nn
+import math
+import os
+import warnings
+from torch.nn.modules.utils import _pair as to_2tuple
+from mmseg.models.builder import BACKBONES
+import torch.nn.functional as F
+from mmcv.cnn import build_norm_layer
+from mmcv.runner import BaseModule
+from mmcv.cnn.bricks import DropPath
+from mmcv.cnn.utils.weight_init import (constant_init, normal_init,
+                                        trunc_normal_init)
+
+
+class Mlp(BaseModule):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Conv2d(in_features, hidden_features, 1)
+        self.dwconv = DWConv(hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Conv2d(hidden_features, out_features, 1)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+
+        x = self.dwconv(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+
+        return x
+
+
+class StemConv(BaseModule):
+    def __init__(self, in_channels, out_channels, norm_cfg=dict(type='SyncBN', requires_grad=True)):
+        super(StemConv, self).__init__()
+
+        self.proj = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels // 2,
+                      kernel_size=(3, 3), stride=(2, 2), padding=(1, 1)),
+            build_norm_layer(norm_cfg, out_channels // 2)[1],
+            nn.GELU(),
+            nn.Conv2d(out_channels // 2, out_channels,
+                      kernel_size=(3, 3), stride=(2, 2), padding=(1, 1)),
+            build_norm_layer(norm_cfg, out_channels)[1],
+        )
+
+    def forward(self, x):
+        x = self.proj(x)
+        _, _, H, W = x.size()
+        x = x.flatten(2).transpose(1, 2)
+        return x, H, W
+
+
+def get_conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias):
+    if type(kernel_size) is int:
+        use_large_impl = kernel_size > 5
+    else:
+        assert len(kernel_size) == 2 and kernel_size[0] == kernel_size[1]
+        use_large_impl = kernel_size[0] > 5
+    has_large_impl = 'LARGE_KERNEL_CONV_IMPL' in os.environ
+    if has_large_impl and in_channels == out_channels and out_channels == groups and use_large_impl and stride == 1 and padding == kernel_size // 2 and dilation == 1:
+        sys.path.append(os.environ['LARGE_KERNEL_CONV_IMPL'])
+        #   Please follow the instructions https://github.com/DingXiaoH/RepLKNet-pytorch/blob/main/README.md
+        #   export LARGE_KERNEL_CONV_IMPL=absolute_path_to_where_you_cloned_the_example (i.e., depthwise_conv2d_implicit_gemm.py)
+        # TODO more efficient PyTorch implementations of large-kernel convolutions. Pull requests are welcomed.
+        # Or you may try MegEngine. We have integrated an efficient implementation into MegEngine and it will automatically use it.
+        from depthwise_conv2d_implicit_gemm import DepthWiseConv2dImplicitGEMM
+        return DepthWiseConv2dImplicitGEMM(in_channels, kernel_size, bias=bias)
+    else:
+        return nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride,
+                         padding=padding, dilation=dilation, groups=groups, bias=bias, padding_mode="replicate")
+
+
+# --------------------------------------------大卷积核实现部分代码
+use_sync_bn = False
+
+
+def enable_sync_bn():
+    global use_sync_bn
+    use_sync_bn = True
+
+
+def get_bn(channels):
+    if use_sync_bn:
+        return nn.SyncBatchNorm(channels)
+    else:
+        return nn.BatchNorm2d(channels)
+
+
+def conv_bn(in_channels, out_channels, kernel_size, stride, padding, groups, dilation=1):
+    if padding is None:
+        padding = kernel_size // 2
+    result = nn.Sequential()
+    result.add_module('conv', get_conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+                                         stride=stride, padding=padding, dilation=dilation, groups=groups, bias=False))
+    result.add_module('bn', get_bn(out_channels))
+    return result
+
+
+def conv_bn_relu(in_channels, out_channels, kernel_size, stride, padding, groups=1, dilation=1):
+    if padding is None:
+        padding = kernel_size // 2
+    result = conv_bn(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+                     stride=stride, padding=padding, groups=groups, dilation=dilation)
+    result.add_module('nonlinear', nn.ReLU())
+    return result
+
+
+def fuse_bn(conv, bn):
+    kernel = conv.weight
+    running_mean = bn.running_mean
+    running_var = bn.running_var
+    gamma = bn.weight
+    beta = bn.bias
+    eps = bn.eps
+    std = (running_var + eps).sqrt()
+    t = (gamma / std).reshape(-1, 1, 1, 1)
+    return kernel * t, beta - running_mean * gamma / std
+
+
+class ReparamLargeKernelConv(nn.Module):
+
+    def __init__(self, in_channels, out_channels, kernel_size,
+                 stride, groups,
+                 dial,
+                 small_kernel,
+                 small_kernel_merged=False,
+                 ):
+        super(ReparamLargeKernelConv, self).__init__()
+        self.kernel_size = kernel_size
+        self.small_kernel = small_kernel
+        padding = kernel_size // 2
+        if kernel_size == 7:
+            self.dial = 4
+            padding = 12
+        elif kernel_size == 9:
+            self.dial = 6
+            padding = 24
+        elif kernel_size == 13:
+            self.dial = 8
+            padding = 48
+        # We assume the conv does not change the feature map size, so padding = k//2. Otherwise, you may configure padding as you wish, and change the padding of small_conv accordingly.
+
+        if small_kernel_merged:
+            self.lkb_reparam = get_conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+                                          stride=stride, padding=padding, dilation=dial, groups=groups, bias=True)
+        else:
+            self.lkb_origin = conv_bn(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+                                      stride=stride, padding=padding, dilation=dial, groups=groups)
+            if small_kernel is not None:
+                assert small_kernel < kernel_size, 'The kernel size for re-param cannot be larger than the large kernel!'
+                self.small_conv = conv_bn(in_channels=in_channels, out_channels=out_channels, kernel_size=small_kernel,
+                                          stride=stride, padding=small_kernel // 2, groups=groups, dilation=1)
+
+    def forward(self, inputs):
+        if hasattr(self, 'lkb_reparam'):
+            out = self.lkb_reparam(inputs)
+        else:
+            out = self.lkb_origin(inputs)
+            # print("input shape===",inputs.shape)
+            # print("-------out..shape==",out.shape)
+            if hasattr(self, 'small_conv'):
+                # print("-------small..shape==", self.small_conv(inputs).shape)
+                out += self.small_conv(inputs)
+        return out
+
+    def get_equivalent_kernel_bias(self):
+        eq_k, eq_b = fuse_bn(self.lkb_origin.conv, self.lkb_origin.bn)
+        if hasattr(self, 'small_conv'):
+            small_k, small_b = fuse_bn(self.small_conv.conv, self.small_conv.bn)
+            eq_b += small_b
+            #   add to the central part
+            eq_k += nn.functional.pad(small_k, [(self.kernel_size - self.small_kernel) // 2] * 4)
+        return eq_k, eq_b
+
+    def merge_kernel(self):
+        eq_k, eq_b = self.get_equivalent_kernel_bias()
+        self.lkb_reparam = get_conv2d(in_channels=self.lkb_origin.conv.in_channels,
+                                      out_channels=self.lkb_origin.conv.out_channels,
+                                      kernel_size=self.lkb_origin.conv.kernel_size, stride=self.lkb_origin.conv.stride,
+                                      padding=self.lkb_origin.conv.padding, dilation=self.lkb_origin.conv.dilation,
+                                      groups=self.lkb_origin.conv.groups, bias=True)
+        self.lkb_reparam.weight.data = eq_k
+        self.lkb_reparam.bias.data = eq_b
+        self.__delattr__('lkb_origin')
+        if hasattr(self, 'small_conv'):
+            self.__delattr__('small_conv')
+
+
+# --------------------------------------------大卷积核实现部分代码
+def structural_reparam(self):
+    for m in self.modules():
+        if hasattr(m, 'merge_kernel'):
+            m.merge_kernel()
+
+
+def shuffle_channels(x, groups=4):
+    """shuffle channels of a 4-D Tensor"""
+    batch_size, channels, height, width = x.size()
+    assert channels % groups == 0
+    channels_per_group = channels // groups
+    # split into groups
+    x = x.view(batch_size, groups, channels_per_group,
+               height, width)
+    # transpose 1, 2 axis
+    x = x.transpose(1, 2).contiguous()
+    # reshape into orignal
+    x = x.view(batch_size, channels, height, width)
+    return x
+
+
+class AttentionModule(BaseModule):
+    def __init__(self, dim, small_kernel=3, small_kernel_merged=False, dial=2):
+        super().__init__()
+        # ----------这里改成  大卷积核实现   先是经过7*7卷积   然后是并行的三个3*3  9*9    13*13 的卷积
+        # 先升维度  然后  通道重排  然后卷积 然后降维度
+
+        self.conv0 = conv_bn_relu(dim, dim, 1, stride=1, padding=0, groups=dim)
+
+        self.kernel_5 = ReparamLargeKernelConv(in_channels=dim, out_channels=dim,
+                                               kernel_size=5,
+                                               stride=1, groups=dim, small_kernel=small_kernel,
+                                               small_kernel_merged=small_kernel_merged, dial=1)
+
+        # self.conv1 = nn.Conv2d(dim, dim, 3, padding=1, groups=dim)
+
+        self.large_kernel_7 = ReparamLargeKernelConv(in_channels=dim, out_channels=dim,
+                                                     kernel_size=7,
+                                                     stride=1, groups=dim, small_kernel=3,
+                                                     small_kernel_merged=small_kernel_merged, dial=4)
+
+        self.large_kernel_9 = ReparamLargeKernelConv(in_channels=dim, out_channels=dim,
+                                                     kernel_size=9,
+                                                     stride=1, groups=dim, small_kernel=3,
+                                                     small_kernel_merged=small_kernel_merged, dial=6)
+
+        self.large_kernel_13 = ReparamLargeKernelConv(in_channels=dim, out_channels=dim,
+                                                      kernel_size=13,
+                                                      stride=1, groups=dim, small_kernel=3,
+                                                      small_kernel_merged=small_kernel_merged, dial=8)
+        """
+        self.conv0 = nn.Conv2d(dim, dim, 5, padding=2, groups=dim)
+
+        self.conv0_1 = nn.Conv2d(dim, dim, (1, 7), padding=(0, 3), groups=dim)
+        self.conv0_2 = nn.Conv2d(dim, dim, (7, 1), padding=(3, 0), groups=dim)
+
+        self.conv1_1 = nn.Conv2d(dim, dim, (1, 11), padding=(0, 5), groups=dim)
+        self.conv1_2 = nn.Conv2d(dim, dim, (11, 1), padding=(5, 0), groups=dim)
+
+        self.conv2_1 = nn.Conv2d(
+            dim, dim, (1, 21), padding=(0, 10), groups=dim)
+        self.conv2_2 = nn.Conv2d(
+            dim, dim, (21, 1), padding=(10, 0), groups=dim)
+
+
+        self.squeeze = nn.AdaptiveAvgPool2d((1, 1))
+        self.compress = nn.Conv2d(dim, dim // 16, 1, 1, 0)
+        self.excitation = nn.Conv2d(dim // 16, dim, 1, 1, 0)
+"""
+
+        self.conv4 = conv_bn_relu(dim, dim, 1, stride=1, padding=0, groups=dim)
+
+    def forward(self, x):
+        uu = x.clone()
+
+        attn = self.conv0(x)
+        # attn = shuffle_channels(attn, self.groups)
+        attn_5 = self.kernel_5(attn)
+
+        attn_7 = self.large_kernel_7(attn)
+
+        attn_9 = self.large_kernel_9(attn)
+
+        attn_13 = self.large_kernel_13(attn)
+
+        attn = attn_5 + attn_7 + attn_9 + attn_13
+
+        attn = self.conv4(attn)
+
+        out = attn * uu
+
+        # out = torch.cat((attn_0, attn_1, attn_3), 1)
+        # out = self.squeeze(out)
+        # out = self.compress(out)
+        # out = torch.relu(out)
+        # out = self.excitation(out)
+        # out = torch.sigmoid(out)
+        # attn = self.conv4(attn)
+
+        return out + x
+
+
+class SpatialAttention(BaseModule):
+    def __init__(self, d_model):
+        super().__init__()
+        self.d_model = d_model
+        self.proj_1 = nn.Conv2d(d_model, d_model, 1)
+        self.activation = nn.GELU()
+        self.spatial_gating_unit = AttentionModule(d_model)
+        self.proj_2 = nn.Conv2d(d_model, d_model, 1)
+
+    def forward(self, x):
+        shorcut = x.clone()
+        x = self.proj_1(x)
+        x = self.activation(x)
+        x = self.spatial_gating_unit(x)
+        x = self.proj_2(x)
+        x = x + shorcut
+        return x
+
+
+class Block(BaseModule):
+
+    def __init__(self,
+                 dim,
+                 mlp_ratio=4.,
+                 drop=0.,
+                 drop_path=0.,
+                 act_layer=nn.GELU,
+                 norm_cfg=dict(type='SyncBN', requires_grad=True)):
+        super().__init__()
+        self.norm1 = build_norm_layer(norm_cfg, dim)[1]
+        self.attn = SpatialAttention(dim)
+        self.drop_path = DropPath(
+            drop_path) if drop_path > 0. else nn.Identity()
+        self.norm2 = build_norm_layer(norm_cfg, dim)[1]
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim,
+                       act_layer=act_layer, drop=drop)
+        layer_scale_init_value = 1e-2
+        self.layer_scale_1 = nn.Parameter(
+            layer_scale_init_value * torch.ones((dim)), requires_grad=True)
+        self.layer_scale_2 = nn.Parameter(
+            layer_scale_init_value * torch.ones((dim)), requires_grad=True)
+
+    def forward(self, x, H, W):
+        B, N, C = x.shape
+        x = x.permute(0, 2, 1).view(B, C, H, W)
+        x = x + self.drop_path(self.layer_scale_1.unsqueeze(-1).unsqueeze(-1)
+                               * self.attn(self.norm1(x)))
+        x = x + self.drop_path(self.layer_scale_2.unsqueeze(-1).unsqueeze(-1)
+                               * self.mlp(self.norm2(x)))
+        x = x.view(B, C, N).permute(0, 2, 1)
+        return x
+
+
+class OverlapPatchEmbed(BaseModule):
+    """ Image to Patch Embedding
+    """
+
+    def __init__(self, patch_size=7, stride=4, in_chans=3, embed_dim=768,
+                 norm_cfg=dict(type='SyncBN', requires_grad=True)):
+        super().__init__()
+        patch_size = to_2tuple(patch_size)
+
+        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=stride,
+                              padding=(patch_size[0] // 2, patch_size[1] // 2))
+        self.norm = build_norm_layer(norm_cfg, embed_dim)[1]
+
+    def forward(self, x):
+        x = self.proj(x)
+        _, _, H, W = x.shape
+        x = self.norm(x)
+
+        x = x.flatten(2).transpose(1, 2)
+
+        return x, H, W
+
+
+@BACKBONES.register_module()
+class MSCAN(BaseModule):
+    def __init__(self,
+                 in_chans=3,
+                 embed_dims=[64, 128, 256, 512],
+                 mlp_ratios=[4, 4, 4, 4],
+                 drop_rate=0.,
+                 drop_path_rate=0.,
+                 depths=[3, 4, 6, 3],
+                 num_stages=4,
+                 norm_cfg=dict(type='SyncBN', requires_grad=True),
+                 pretrained=None,
+                 init_cfg=None):
+        super(MSCAN, self).__init__(init_cfg=init_cfg)
+
+        assert not (init_cfg and pretrained), \
+            'init_cfg and pretrained cannot be set at the same time'
+        if isinstance(pretrained, str):
+            warnings.warn('DeprecationWarning: pretrained is deprecated, '
+                          'please use "init_cfg" instead')
+            self.init_cfg = dict(type='Pretrained', checkpoint=pretrained)
+        elif pretrained is not None:
+            raise TypeError('pretrained must be a str or None')
+
+        self.depths = depths
+        self.num_stages = num_stages
+
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate,
+                                                sum(depths))]  # stochastic depth decay rule
+        cur = 0
+
+        for i in range(num_stages):
+            if i == 0:
+                patch_embed = StemConv(3, embed_dims[0], norm_cfg=norm_cfg)
+            else:
+                patch_embed = OverlapPatchEmbed(patch_size=7 if i == 0 else 3,
+                                                stride=4 if i == 0 else 2,
+                                                in_chans=in_chans if i == 0 else embed_dims[i - 1],
+                                                embed_dim=embed_dims[i],
+                                                norm_cfg=norm_cfg)
+
+            block = nn.ModuleList([Block(dim=embed_dims[i], mlp_ratio=mlp_ratios[i],
+                                         drop=drop_rate, drop_path=dpr[cur + j],
+                                         norm_cfg=norm_cfg)
+                                   for j in range(depths[i])])
+            norm = nn.LayerNorm(embed_dims[i])
+            cur += depths[i]
+
+            setattr(self, f"patch_embed{i + 1}", patch_embed)
+            setattr(self, f"block{i + 1}", block)
+            setattr(self, f"norm{i + 1}", norm)
+
+    def init_weights(self):
+        print('init cfg', self.init_cfg)
+        if self.init_cfg is None:
+            for m in self.modules():
+                if isinstance(m, nn.Linear):
+                    trunc_normal_init(m, std=.02, bias=0.)
+                elif isinstance(m, nn.LayerNorm):
+                    constant_init(m, val=1.0, bias=0.)
+                elif isinstance(m, nn.Conv2d):
+                    fan_out = m.kernel_size[0] * m.kernel_size[
+                        1] * m.out_channels
+                    fan_out //= m.groups
+                    normal_init(
+                        m, mean=0, std=math.sqrt(2.0 / fan_out), bias=0)
+        else:
+
+            super(MSCAN, self).init_weights()
+
+    def forward(self, x):
+        B = x.shape[0]
+        outs = []
+
+        for i in range(self.num_stages):
+            patch_embed = getattr(self, f"patch_embed{i + 1}")
+            block = getattr(self, f"block{i + 1}")
+            norm = getattr(self, f"norm{i + 1}")
+            x, H, W = patch_embed(x)
+            for blk in block:
+                x = blk(x, H, W)
+            x = norm(x)
+            x = x.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+            outs.append(x)
+
+        return outs
+
+
+class DWConv(nn.Module):
+    def __init__(self, dim=768):
+        super(DWConv, self).__init__()
+        self.dwconv = nn.Conv2d(dim, dim, 3, 1, 1, bias=True, groups=dim)
+
+    def forward(self, x):
+        x = self.dwconv(x)
+        return x
